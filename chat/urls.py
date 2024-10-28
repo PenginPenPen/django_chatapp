acd84dbb20cc209b6f6ed71771c581d1f19934a4
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-10-28 11:35:29 +0900

    Commit

--- chat/urls.py
+++ chat/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('chatapp.urls')),
-] 
+    path('chat/', include('chatapp.urls')),
+]
