e0db4feab3ebb1a0e7157c7bbd6af0ec667e7053
Tomoya <92212932+PenginPenPen@users.noreply.github.com>
2024-12-09 10:17:40 +0900

    設定ファイルの変更

--- chat/settings.py
+++ chat/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'chatapp'
+    'chat.apps.ChatConfig'
 ]
 
 MIDDLEWARE = [
